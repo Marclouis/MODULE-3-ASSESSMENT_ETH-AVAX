105457367517c84961d2d0e87494c17a02cd8d7f
Marclouis <137591203+Marclouis@users.noreply.github.com>
2024-05-14 18:58:48 +0800

    Create MODULE3.sol

---
+++ MODULE3.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract Mtoken is ERC20  {
+    constructor() ERC20 ("MY TOKEN","MT") {}
+
+         function mint() public  {
+        _mint(msg.sender, 50 * 10**18);
+    }
+        function burn(address from, uint256 amount) public {
+            _burn(from, amount);
+        }
+}
